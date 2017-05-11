f8fe877c912e5986e4fe0942694578984eaa5576
tactreconx <league@WTS-MM-11.local>
2017-05-11 14:24:34 -0700

    I did xGeeks and change calculator!

--- src/variables_and_conditionals/ChangeCalculator.java
+++ src/variables_and_conditionals/ChangeCalculator.java
@@ -17,16 +17,21 @@ public class ChangeCalculator {
     public static void main(String[] args) {
 
         // Ask the user how many nickels they have
-
+String Nickels = JOptionPane.showInputDialog(null, "How many nickels do you have?");
         // Convert their answer to an int using Integer.parseInt()
-
+int Nickel = Integer.parseInt(Nickels);
         // Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog(null, "How many dimes do you have?");
+int dime = Integer.parseInt(dimes);
         // Ask the user how many quarters they have, and convert their answer
-
+String quarters = JOptionPane.showInputDialog(null, "How many quarters do you have?");
+int quarter = Integer.parseInt(quarters);
         // Calculate how much money the user has and save it in a double variable 
-
+double totalnickels = (double)(Nickel * 0.05);
+double totaldimes = (double)(dime * 0.10);
+double totalquarters = (double)(quarter * 0.25);
         // Tell the user how much money they have
-
+double total = totaldimes + totalnickels + totalquarters; 
+JOptionPane.showMessageDialog(null, "You have won! $" + (total));
     }
 }
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,20 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+String JosesPower = "Being majestic";
+String HelensPower = "Eat";
+String JamiesPower = "Sleep";
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
+String NameAnswer = JOptionPane.showInputDialog("Enter your name.");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
+if(NameAnswer.equals("Jamie")){
+	JOptionPane.showMessageDialog(null, JamiesPower);
+}
+if(NameAnswer.equals("Jose")){
+	JOptionPane.showMessageDialog(null, JosesPower);
+}
+if(NameAnswer.equalsIgnoreCase("Helen")){
+	JOptionPane.showMessageDialog(null, HelensPower);
+}
     }
 }
